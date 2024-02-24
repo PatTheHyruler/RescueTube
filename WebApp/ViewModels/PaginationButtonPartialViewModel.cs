a217f6ef49c095a320c8807d4604b718c1d1e1fc
patthehyruler <pmcroofplays@gmail.com>
2024-02-24 12:47:15 +0200

    Pagination UI fixes

--- WebApp/ViewModels/PaginationButtonPartialViewModel.cs
+++ WebApp/ViewModels/PaginationButtonPartialViewModel.cs
@@ -1,3 +1,3 @@
 ﻿namespace WebApp.ViewModels;
 
-public record PaginationButtonPartialViewModel(int Page, int Limit, string Text);
+public record PaginationButtonPartialViewModel(int Page, int Limit, string Text, bool Disabled = false);
