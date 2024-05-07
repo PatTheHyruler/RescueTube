901b7245df7a8f9c6eb62b3b96567b31b1d750c2
patthehyruler <pmcroofplays@gmail.com>
2024-05-07 19:53:02 +0300

    InfoJson field as JSONB column in Postgres

--- DAL.EF/DbContexts/PostgresAppDbContext.cs
+++ DAL.EF/DbContexts/PostgresAppDbContext.cs
@@ -1,4 +1,5 @@
 using DAL.Contracts;
+using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
@@ -11,5 +12,11 @@ public class PostgresAppDbContext : AbstractAppDbContext
         IOptions<DbLoggingOptions> dbLoggingOptions, ILoggerFactory? loggerFactory = null) :
         base(options, dbLoggingOptions, loggerFactory)
     {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.Entity<Video>().Property(v => v.InfoJson).HasColumnType("jsonb");
     }
 }
