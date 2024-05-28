5e88d451d0f535ba5116e22b57f9c5bde3761378
patthehyruler <pmcroofplays@gmail.com>
2024-05-29 02:15:32 +0300

    IFetchable extension methods for generic latest fetch

---
+++ RescueTube.Core/Utils/DomainExtensions.cs
@@ -0,0 +1,18 @@
+﻿using RescueTube.Domain.Contracts;
+
+namespace RescueTube.Core.Utils;
+
+public static class DomainExtensions
+{
+    private static DateTimeOffset? GetLatest(DateTimeOffset? a, DateTimeOffset? b) =>
+        // Perhaps should use IEnumerable.Max() and a params[] argument instead?
+        a > b
+            ? a
+            : b ?? a;
+
+    public static DateTimeOffset? LastFetch(this IFetchable entity) =>
+        GetLatest(entity.LastFetchOfficial, entity.LastFetchUnofficial);
+
+    public static DateTimeOffset? LastSuccessfulFetch(this IFetchable entity) =>
+        GetLatest(entity.LastSuccessfulFetchOfficial, entity.LastSuccessfulFetchUnofficial);
+}
