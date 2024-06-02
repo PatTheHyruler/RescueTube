3111b549104d8ab3d8fcd0865ed8d7cce240b751
patthehyruler <pmcroofplays@gmail.com>
2024-06-02 10:35:40 +0300

    Basic video details endpoint

--- WebApp/ApiControllers/VideoController.cs
+++ WebApp/ApiControllers/VideoController.cs
@@ -12,7 +12,7 @@ namespace WebApp.ApiControllers;
 
 [ApiVersion("1.0")]
 [ApiController]
-[Route("api/v{version:apiVersion}/videos/[action]")]
+[Route("api/v{version:apiVersion}/videos")]
 public class VideoController : ControllerBase
 {
     private readonly ServiceUow _serviceUow;
@@ -29,7 +29,7 @@ public class VideoController : ControllerBase
     /// <response code="200">List of videos matching the query</response>
     [SwaggerResponse(StatusCodes.Status200OK, null, typeof(VideoSearchResponseDtoV1))]
     [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-    [HttpPost]
+    [HttpPost("[action]")]
     public async Task<ActionResult> Search([FromBody] VideoSearchDtoV1 query)
     {
         var response = await _serviceUow.VideoPresentationService.SearchVideosAsync(
@@ -45,5 +45,39 @@ public class VideoController : ControllerBase
             Videos = response.Result.Select(v => v.MapToVideoSimpleDtoV1(HttpContext.GetBaseUrl())),
             PaginationResult = response.PaginationResult,
         });
+    }
+
+    /// <summary>
+    /// Get video data.
+    /// </summary>
+    /// <param name="videoId">Video ID</param>
+    /// <returns>Video data</returns>
+    /// <response code="200">Video details fetched successfully.</response>
+    /// <response code="403">Access to video forbidden.</response>
+    /// <response code="404">Video not found.</response>
+    [SwaggerResponse(StatusCodes.Status200OK, null, typeof(VideoSimpleDtoV1))]
+    [SwaggerResponse(StatusCodes.Status403Forbidden)]
+    [SwaggerErrorResponse(StatusCodes.Status404NotFound)]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [AllowAnonymous]
+    [HttpGet("{videoId:guid}")]
+    public async Task<ActionResult<VideoSimpleDtoV1>> Index([FromRoute] Guid videoId)
+    {
+        if (!await _serviceUow.AuthorizationService.IsVideoAccessAllowed(videoId, User))
+        {
+            return Forbid();
+        }
+
+        var response = await _serviceUow.VideoPresentationService.GetVideoSimple(videoId);
+        if (response == null)
+        {
+            return NotFound(new ErrorResponseDto
+            {
+                ErrorType = EErrorType.EntityNotFound,
+                Message = $"Video {videoId} not found",
+            });
+        }
+
+        return Ok(response.MapToVideoSimpleDtoV1(HttpContext.GetBaseUrl()));
     }
 }
