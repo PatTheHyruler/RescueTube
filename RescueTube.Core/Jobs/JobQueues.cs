0b5cff0663b96d3805cbecf0a1ecfb2978255722
PatTheHyruler <pmcroofplays@gmail.com>
2024-09-28 20:38:18 +0300

    Fixed job queue processing order?

--- RescueTube.Core/Jobs/JobQueues.cs
+++ RescueTube.Core/Jobs/JobQueues.cs
@@ -2,11 +2,13 @@ namespace RescueTube.Core.Jobs;
 
 public static class JobQueues
 {
-    public const string Critical = "priority_critical";
-    public const string HighPriority = "priority_high";
+    // Unfortunately queue processing order is storage-specific
+    // In the case of PostgreSQL, it seems to be alphabetical ascending order?
+    public const string Critical = "a1000_priority_critical";
+    public const string HighPriority = "a2000_priority_high";
     public const string Default = "default";
-    public const string LowPriority = "priority_low";
-    public const string LowerPriority = "priority_lower";
+    public const string LowPriority = "z1000_priority_low";
+    public const string LowerPriority = "z2000_priority_lower";
 
     public static string[] Queues = [Critical, HighPriority, Default, LowPriority, LowerPriority];
 }
