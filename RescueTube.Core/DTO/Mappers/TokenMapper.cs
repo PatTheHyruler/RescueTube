e1af6f16108d232b7d3d1d4b873d43daf219c4ad
PatTheHyruler <55945965+PatTheHyruler@users.noreply.github.com>
2025-02-23 18:19:56 +0200

    Use manual mapping for RefreshToken, remove unused code (#45)

--- RescueTube.Core/DTO/Mappers/TokenMapper.cs
+++ RescueTube.Core/DTO/Mappers/TokenMapper.cs
@@ -1,11 +1,17 @@
 using RescueTube.Core.DTO.Entities.Identity;
-using Riok.Mapperly.Abstractions;
 
 namespace RescueTube.Core.DTO.Mappers;
 
-[Mapper]
-public static partial class TokenMapper
+public static class TokenMapper
 {
-    public static partial Domain.Entities.Identity.RefreshToken ToDomainToken(this RefreshToken token);
-    public static partial RefreshToken ToBllToken(this Domain.Entities.Identity.RefreshToken token);
+    public static Domain.Entities.Identity.RefreshToken ToDomainToken(this RefreshToken token)
+    {
+        return new()
+        {
+            UserId = token.UserId,
+            Token = token.Token,
+            ExpiresAt = token.ExpiresAt,
+            JwtHash = token.JwtHash,
+        };
+    }
 }
