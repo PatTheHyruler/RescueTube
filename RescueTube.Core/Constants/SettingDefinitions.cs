689489c546d47c978bbe9f72ff21e1d5eb87b3b3
PatTheHyruler <pmcroofplays@gmail.com>
2025-06-11 00:34:35 +0300

    Add automatic killswitch for large datafetch count

--- RescueTube.Core/Constants/SettingDefinitions.cs
+++ RescueTube.Core/Constants/SettingDefinitions.cs
@@ -12,8 +12,16 @@ public static class SettingDefinitions
     public static readonly SettingDefinition.Bool.WithDefault DisableAllArchival =
         new("DisableAllArchival", false);
 
+    public static readonly SettingDefinition.Long.WithDefault DataFetchKillSwitchCutoffMinutes =
+        new("DataFetchKillSwitchCutoffMinutes", 60);
+
+    public static readonly SettingDefinition.Long.WithDefault DataFetchKillSwitchLimit =
+        new("DataFetchKillSwitchLimit", 100);
+
     public static readonly FrozenSet<SettingDefinition> AllDefinitions = [
         MinFreeSpaceForVideoDownload,
         DisableAllArchival,
+        DataFetchKillSwitchCutoffMinutes,
+        DataFetchKillSwitchLimit,
     ];
 }
