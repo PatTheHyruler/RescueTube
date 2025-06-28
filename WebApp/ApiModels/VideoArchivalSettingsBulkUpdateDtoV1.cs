e57bb9f2faafa91650db7722b30eaee68110ec88
PatTheHyruler <pmcroofplays@gmail.com>
2025-06-29 00:18:21 +0300

    Initial work on Partial source generator

--- WebApp/ApiModels/VideoArchivalSettingsBulkUpdateDtoV1.cs
+++ WebApp/ApiModels/VideoArchivalSettingsBulkUpdateDtoV1.cs
@@ -1,9 +1,14 @@
+using RescueTube.Core.SourceGeneration;
+
 namespace WebApp.ApiModels;
+
+[Partial<VideoArchivalSettingsDtoV1>]
+public partial record VideoArchivalSettingsPartialDtoV1;
 
 public record VideoArchivalSettingsBulkUpdateDtoV1
 {
     public required VideoSearchFilterDtoV1? Filter { get; init; }
     public Guid[]? VideoIds { get; init; }
 
-    public required VideoArchivalSettingsDtoV1 Settings { get; init; }
+    public required VideoArchivalSettingsPartialDtoV1 Settings { get; init; }
 }
