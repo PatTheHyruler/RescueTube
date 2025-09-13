1c3906fefabd3510a723152e4fafe1e7b4a14424
PatTheHyruler <pmcroofplays@gmail.com>
2025-09-13 18:54:15 +0300

    Remove unused WebApp utils

--- RescueTube.WebApi/Utils/SwaggerErrorResponseAttribute.cs
+++
@@ -1,7 +0,0 @@
-﻿using RescueTube.WebApi.ApiModels;
-using Swashbuckle.AspNetCore.Annotations;
-
-namespace RescueTube.WebApi.Utils;
-
-public class SwaggerErrorResponseAttribute(int statusCode)
-    : SwaggerResponseAttribute(statusCode, null, typeof(ErrorResponseDto));
