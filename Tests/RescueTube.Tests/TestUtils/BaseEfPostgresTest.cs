1521f419a60706af1c0c83c8b9d464e573eb4f27
PatTheHyruler <pmcroofplays@gmail.com>
2026-01-10 19:19:59 +0200

    Cleanup some warnings

--- Tests/RescueTube.Tests/TestUtils/BaseEfPostgresTest.cs
+++ Tests/RescueTube.Tests/TestUtils/BaseEfPostgresTest.cs
@@ -2,7 +2,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Time.Testing;
-using RescueTube.Core;
 using RescueTube.Core.Data;
 using RescueTube.DAL.EF.Postgres;
 
